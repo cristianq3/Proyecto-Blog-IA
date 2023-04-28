0128df518367206f59b4c973ab4d6099e796b1e1
cristianq3 <cristianq3@gmail.com>
2023-04-28 17:59:06 -0300

    Se crea estructura de Article comentarios

---
+++ js/comentarios.js
@@ -0,0 +1,17 @@
+const FormComentarios = document.querySelector("#formComentarios");
+
+FormComentarios.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const nombre = document.getElementById("nombre").value;
+  const mail = document.getElementById("mail").value;
+  const comentario = document.getElementById("comentario").value;
+
+  });
+
+function agregarComentario() {
+
+
+
+
+
+}
